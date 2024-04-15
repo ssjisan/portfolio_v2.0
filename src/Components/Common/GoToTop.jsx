7ebf2857ad2f6846f7beadb22b59e155b393550a
ssjisan <ssjisan.dev@gmail.com>
2024-04-15 15:08:12 +0600

    tooltip added

--- src/Components/Common/GoToTop.jsx
+++ src/Components/Common/GoToTop.jsx
@@ -1,4 +1,4 @@
-import { Box } from "@mui/material";
+import { Box, Tooltip } from "@mui/material";
 import { useContext } from "react";
 import { DataContext } from "../../DataProcessing/DataProcessing";
 import { TopArrow } from "../../assets/Icons";
@@ -7,37 +7,39 @@ export default function GoToTop() {
   const { goToTop, light } = useContext(DataContext);
 
   return (
-    <Box
-      sx={{
-        position: "fixed",
-        bottom: {
-          xs: 32,
-          sm: 40,
-          md: 48,
-        },
-        right: {
-          xs: 16,
-          sm: 24,
-          md: 48,
-        },
-        zIndex: 1000,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        border: light
-          ? "1px solid rgba(255, 255, 255, 0.24)"
-          : "1px solid rgba(17, 24, 39, 0.16)",
-        height: "40px",
-        width: "40px",
-        borderRadius: "8px",
-        cursor: "pointer",
-        background: light
-          ? "rgba(255, 255, 255, 0.12)"
-          : "rgba(17, 24, 39, 0.08)",
-      }}
-      onClick={goToTop}
-    >
-      <TopArrow color={light ? "#fff" : "#121120"} />
-    </Box>
+    <Tooltip title="Go To Top">
+      <Box
+        sx={{
+          position: "fixed",
+          bottom: {
+            xs: 32,
+            sm: 40,
+            md: 48,
+          },
+          right: {
+            xs: 16,
+            sm: 24,
+            md: 48,
+          },
+          zIndex: 1000,
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          border: light
+            ? "1px solid rgba(255, 255, 255, 0.24)"
+            : "1px solid rgba(17, 24, 39, 0.16)",
+          height: "40px",
+          width: "40px",
+          borderRadius: "8px",
+          cursor: "pointer",
+          background: light
+            ? "rgba(255, 255, 255, 0.12)"
+            : "rgba(17, 24, 39, 0.08)",
+        }}
+        onClick={goToTop}
+      >
+        <TopArrow color={light ? "#fff" : "#121120"} />
+      </Box>
+    </Tooltip>
   );
 }
--- src/Layout/Navbar.jsx
+++ src/Layout/Navbar.jsx
@@ -1,81 +1,127 @@
-import { Box, Container, IconButton, useMediaQuery } from "@mui/material"
+import {
+  Box,
+  Container,
+  IconButton,
+  Tooltip,
+  useMediaQuery,
+} from "@mui/material";
 import { Link, useLocation } from "react-router-dom";
 import { useContext } from "react";
 import { DataContext } from "../DataProcessing/DataProcessing";
 // import Logo from "../assets/Logo";
 import { Menu, Moon, Sun } from "../assets/Icons";
 import MenuDrawer from "./MenuDrawer";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 export default function Navbar() {
-    const { goToTop, handleMode, light, toggleDrawer, open } = useContext(DataContext);
-    // eslint-disable-next-line
-    const { pathname } = useLocation();
+  const { goToTop, handleMode, light, toggleDrawer, open } =
+    useContext(DataContext);
+  // eslint-disable-next-line
+  const { pathname } = useLocation();
 
-    // eslint-disable-next-line
-    const forBelow768 = useMediaQuery("(max-width:768px)");
-    const forBelow467 = useMediaQuery("(max-width:467px)");
+  // eslint-disable-next-line
+  const forBelow768 = useMediaQuery("(max-width:768px)");
+  const forBelow467 = useMediaQuery("(max-width:467px)");
 
-    // Configure Style Start
+  // Configure Style Start
 
-    const NavSx = {
-        p: "16px 0px",
-        display: "flex",
-        justifyContent: "space-between",
-        alignItems: "center",
-        position: "fixed",
-        width: "100%",
-        zIndex: 1000,
-        background: pathname === "/" ? "transparent" : (light ? "rgba(18, 17, 32, 0.48)" : "rgba(255, 255, 255, 0.48)"),
-        backdropFilter: pathname !== "/" && "blur(16px)",
-    }
-    const LogoSx = {
-        width: forBelow768 ? "140px" : "196px",
-    }
+  const NavSx = {
+    p: "16px 0px",
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+    position: "fixed",
+    width: "100%",
+    zIndex: 1000,
+    background:
+      pathname === "/"
+        ? "transparent"
+        : light
+        ? "rgba(18, 17, 32, 0.48)"
+        : "rgba(255, 255, 255, 0.48)",
+    backdropFilter: pathname !== "/" && "blur(16px)",
+  };
+  const LogoSx = {
+    width: forBelow768 ? "140px" : "196px",
+  };
 
-    const MenuButtonSx = {
-        display: "flex",
-        height: forBelow467 ? "36px" : "40px",
-        width: forBelow467 ? "36px" : "40px",
-        justifyContent: "center",
-        alignItems: "center",
-        borderRadius: "8px",
-    }
-    const StyleSx = { display: "flex", justifyContent: "space-between", alignItems: "center" }
-    // Configure Style End
+  const MenuButtonSx = {
+    display: "flex",
+    height: forBelow467 ? "36px" : "40px",
+    width: forBelow467 ? "36px" : "40px",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: "8px",
+  };
+  const StyleSx = {
+    display: "flex",
+    justifyContent: "space-between",
+    alignItems: "center",
+  };
+  // Configure Style End
 
-    return (
-        <motion.div
-            initial={{ y: -100, opacity: .5 }}
-            animate={{ y: 0, opacity: 1 }}
-            transition={{
-                duration: 0.5
-            }}
-        >
-            <Box sx={NavSx}>
-                <Container sx={StyleSx} >
-                    <Link to="/">
-                        <Box sx={LogoSx} onClick={goToTop}>
-                            {light ? <img src={"/lightLogo.svg"} width="100%" />
-                                :
-                                <img src={"/Logo.svg"} width="100%" />}
-                        </Box>
-                    </Link>
-                    <Box sx={{ display: "flex", gap: "8px" }}>
-                        {light ? <IconButton sx={{ ...MenuButtonSx, border: "1px solid rgba(255, 255, 255, 0.32)", }} onClick={handleMode}>
-                            <Sun />
-                        </IconButton>
-                            :
-                            <IconButton sx={{ ...MenuButtonSx, border: "1px solid rgba(17, 24, 39, 0.32)", }} onClick={handleMode}>
-                                <Moon />
-                            </IconButton>}
-                        <IconButton sx={{ ...MenuButtonSx, background: light ? "rgba(255, 255, 255, 0.12)" : "rgba(17, 24, 39, 0.08)", }} onClick={toggleDrawer}>
-                            <Menu />
-                        </IconButton>
-                    </Box>
-                </Container>
-                <MenuDrawer open={open} />
+  return (
+    <motion.div
+      initial={{ y: -100, opacity: 0.5 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{
+        duration: 0.5,
+      }}
+    >
+      <Box sx={NavSx}>
+        <Container sx={StyleSx}>
+          <Link to="/">
+            <Box sx={LogoSx} onClick={goToTop}>
+              {light ? (
+                <img src={"/lightLogo.svg"} width="100%" />
+              ) : (
+                <img src={"/Logo.svg"} width="100%" />
+              )}
             </Box>
-        </motion.div>
-    )
+          </Link>
+          <Box sx={{ display: "flex", gap: "8px" }}>
+            {light ? (
+              <Tooltip title="Go Light">
+                <IconButton
+                  sx={{
+                    ...MenuButtonSx,
+                    border: "1px solid rgba(255, 255, 255, 0.32)",
+                  }}
+                  onClick={handleMode}
+                >
+                  <Sun />
+                </IconButton>
+              </Tooltip>
+            ) : (
+              <Tooltip title="Go Dark">
+                <IconButton
+                  sx={{
+                    ...MenuButtonSx,
+                    border: "1px solid rgba(17, 24, 39, 0.32)",
+                  }}
+                  onClick={handleMode}
+                >
+                  <Moon />
+                </IconButton>
+              </Tooltip>
+            )}
+            <Tooltip title="Menu">
+              <IconButton
+                sx={{
+                  ...MenuButtonSx,
+                  background: light
+                    ? "rgba(255, 255, 255, 0.12)"
+                    : "rgba(17, 24, 39, 0.08)",
+                }}
+                onClick={toggleDrawer}
+              >
+                <Menu />
+              </IconButton>
+            </Tooltip>
+          </Box>
+        </Container>
+        <MenuDrawer open={open} />
+      </Box>
+    </motion.div>
+  );
 }
